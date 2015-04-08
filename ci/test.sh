3cb7945822712d2984f9ca340c4be2297ded0ac4
Jerry Zhao <zhaoxinyu@huawei.com>
2015-04-08 10:19:03 +0800

    regtest commissioning
    installation of os_only cento6.5 is successful
    going to test Ubuntu 14.04
    
    Change-Id: Ib08cf2860ae28a219f259873fcc015442ff6fc67

--- ci/test.sh
+++ ci/test.sh
@@ -43,6 +43,15 @@ if [[ -n $mac_array ]]
     echo "bringing up pxe boot vms"
     i=0
     for mac in "$mac_array"; do
+        virsh list |grep pxe${i}
+        if [[ $? == 0 ]]; then
+            virsh destroy pxe${i}
+            virsh undefine pxe${i}
+        fi
+        virsh list --all |grep pxe${i}
+        if [[ $? == 0 ]]; then
+            virsh undefine pxe${i}
+        fi
         echo "creating vm disk for instance pxe${i}"
         sudo qemu-img create -f raw /home/pxe${i}.raw ${VIRT_DISK}
         sudo virt-install --accelerate --hvm --connect qemu:///system \
@@ -66,18 +75,53 @@ if [[ -n $mac_array ]]
         echo "add network boot option and make pxe${i} reboot if failing"
         sudo sed -i "/<boot dev='hd'\/>/ a\    <boot dev='network'\/>" /etc/libvirt/qemu/pxe${i}.xml
         sudo sed -i "/<boot dev='network'\/>/ a\    <bios useserial='yes' rebootTimeout='0'\/>" /etc/libvirt/qemu/pxe${i}.xml
-        sudo virsh define pxe${i}
+        sudo virsh define /etc/libvirt/qemu/pxe${i}.xml
         sudo virsh start pxe${i}
         let i=i+1
     done
 fi
 
-sudo vagrant up regtest_vm
-if [[ $? != 0 ]]; then
-    sudo vagrant provision regtest_vm
-    if [[ $? != 0 ]]; then
-        echo "deployment of cluster failed"
-        exit 1
-    fi
+rm -rf compass-core
+git clone http://git.openstack.org/stackforge/compass-core -b dev/experimental
+cd compass-core
+virtualenv venv
+source venv/bin/activate
+pip install -e .
+if [[ ! -f /var/log/compass ]]; then
+    sudo mkdir /var/log/compass
+    sudo chown -R 777 /var/log/compass
 fi
-echo "deployment of cluster complete"
+if [[ ! -f /etc/compass ]]; then
+    sudo mkdir /etc/compass
+    sudo cp -rf conf/setting /etc/compass/.
+fi
+cp bin/switch_virtualenv.py.template bin/switch_virtualenv.py
+sed -i "s|\$PythonHome|$VIRTUAL_ENV|g" /opt/compass/bin/switch_virtualenv.py
+source ../compass-install/ci/allinone.conf
+bin/client.py --logfile= --loglevel=debug --logdir= --compass_server="${COMPASS_SERVER_URL}" \
+--compass_user_email="${COMPASS_USER_EMAIL}" --compass_user_password="${COMPASS_USER_PASSWORD}" \
+--cluster_name="${CLUSTER_NAME}" --language="${LANGUAGE}" --timezone="${TIMEZONE}" \
+--hostnames="${HOSTNAMES}" --partitions="${PARTITIONS}" --subnets="${SUBNETS}" \
+--adapter_os_pattern="${ADAPTER_OS_PATTERN}" --adapter_name="${ADAPTER_NAME}" \
+--adapter_target_system_pattern="${ADAPTER_TARGET_SYSTEM_PATTERN}" \
+--adapter_flavor_pattern="${ADAPTER_FLAVOR_PATTERN}" \
+--http_proxy="${PROXY}" --https_proxy="${PROXY}" --no_proxy="${IGNORE_PROXY}" \
+--ntp_server="${NTP_SERVER}" --dns_servers="${NAMESERVERS}" --domain="${DOMAIN}" \
+--search_path="${SEARCH_PATH}" --default_gateway="${GATEWAY}" \
+--server_credential="${SERVER_CREDENTIAL}" --local_repo_url="${LOCAL_REPO_URL}" \
+--os_config_json_file="${OS_CONFIG_FILENAME}" --service_credentials="${SERVICE_CREDENTIALS}" \
+--console_credentials="${CONSOLE_CREDENTIALS}" --host_networks="${HOST_NETWORKS}" \
+--network_mapping="${NETWORK_MAPPING}" --package_config_json_file="${PACKAGE_CONFIG_FILENAME}" \
+--host_roles="${HOST_ROLES}" --default_roles="${DEFAULT_ROLES}" --switch_ips="${SWITCH_IPS}" \
+--machines="${machines}" --switch_credential="${SWITCH_CREDENTIAL}" \
+--deployment_timeout="${DEPLOYMENT_TIMEOUT}" --${POLL_SWITCHES_FLAG} --dashboard_url="${DASHBOARD_URL}"
+#sudo vagrant up regtest_vm
+#if [[ $? != 0 ]]; then
+#    sudo vagrant provision regtest_vm
+#    if [[ $? != 0 ]]; then
+#        echo "deployment of cluster failed"
+#        exit 1
+#    fi
+#fi
+#echo "deployment of cluster complete"
+
