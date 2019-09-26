77afb388947e81149ab3bfedf36b8395f827c267
Thierry Carrez <thierry@openstack.org>
2019-09-26 16:33:53 +0200

    Retire github mirror, repo moved to opendev

--- docker/build/compass/scripts/refresh.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-set -e
-/opt/compass/bin/manage_db.py createdb
-# /opt/compass/bin/clean_installers.py
-# /opt/compass/bin/clean_installation_logs.py
-/usr/sbin/apachectl -D NO_DETACH -D FOREGROUND
-/usr/bin/redis-server &
-CELERY_CONFIG_MODULE=compass.utils.celeryconfig_wrapper C_FORCE_ROOT=1 /opt/compass/bin/celery worker &> /tmp/celery-worker.log &
-/opt/compass/bin/progress_update.py &> /tmp/progress_update.log
